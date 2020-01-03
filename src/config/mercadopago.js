0d2187a6c879e7acc7a13d4bf490d8644c0861ac
rafazx <rafael.wogel@hotmail.com>
2020-01-03 14:41:06 -0300

    correação autenticação

--- src/config/mercadopago.js
+++ src/config/mercadopago.js
@@ -1,5 +1,5 @@
 module.exports =
 {
     "sandbox" : true,
-    "access_token" : process.env.ACESS_TOKEN_MERCADO_PAGO //|| "TEST-1349754177677345-122916-733c4c3ce2d52b7307ed4202f4739ba9-507820401",
+    "access_token" : process.env.ACESS_TOKEN_MERCADO_PAGO || "TEST-1349754177677345-122916-733c4c3ce2d52b7307ed4202f4739ba9-507820401",
 }
